c90431053de0089e145da08d6c4e2f4145cec0e4
Erwin Wolff <erwinwolff@hotmail.com>
2020-03-22 12:25:32 +0100

    apxs and cmake configuration

--- apxs.sh
+++ apxs.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/apxs -l mysqlclient -n mod_whitelist_mysql -i -a -c  src/mod_whitelist_mysql.c src/CConfigUtil.cpp  src/CGetIpAddressList.cpp src/CConfigUtil.h  src/CGetIpAddressList.h
+/usr/bin/apxs -l mysqlclient -n whitelist_mysql -i -a -c  src/mod_whitelist_mysql.c src/CConfigUtil.cpp src/CGetIpAddressList.cpp src/CConfigUtil.h src/CGetIpAddressList.h
+/usr/bin/apxs -l mysqlclient -n blacklist_mysql -i -a -c  src/mod_blacklist_mysql.c src/CConfigUtil.cpp src/CGetIpAddressList.cpp src/CConfigUtil.h src/CGetIpAddressList.h
