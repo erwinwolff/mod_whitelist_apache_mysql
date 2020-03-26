333760b5f38db2d610c75ffa3f6cf7a9ae1666de
Erwin Wolff <erwinwolff@hotmail.com>
2020-03-26 13:10:43 -0400

    working with configuration

--- src/mod_whitelist_mysql.c
+++ src/mod_whitelist_mysql.c
@@ -19,6 +19,8 @@
 void          *mysql_whitelist_create_dir_conf(apr_pool_t *pool, char *context);
 void          *mysql_whitelist_merge_dir_conf(apr_pool_t *pool, void *BASE, void *ADD);
 
+static server_rec * server = NULL;
+
 module AP_MODULE_DECLARE_DATA whitelist_mysql_module;
 
 typedef struct {
@@ -32,85 +34,87 @@ void *mysql_whitelist_create_dir_conf(apr_pool_t *pool, char *context)
 {
 	context = context ? context : "(undefined context)";
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "Standard MySql Whitelist configuration being created ...");
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "Standard MySql Whitelist configuration being created ...");
 
-	whitelist_mysql_config *cfg = apr_pcalloc(pool, sizeof(whitelist_mysql_config));
-	if (cfg)
+	whitelist_mysql_config *conf = apr_pcalloc(pool, sizeof(whitelist_mysql_config));
+	if (conf)
 	{
-		char * connectionString = "Server=localhost;Database=Testdb;User=test;Password=test;Port=3306";
-		char * query =  "SELECT Ip FROM allowedips";
+//		char * connectionString = "Server=localhost;Database=Testdb;User=test;Password=test;Port=3306";
+//		char * query =  "SELECT Ip FROM allowedips";
 
-		strcpy(cfg->context, context);
-		/* Set some default values */
-		cfg->enabled = 1;
-		strcpy(cfg->connectionString, connectionString);
-		strcpy(cfg->query, query);
-
-		//	cfg->query = "Server=localhost;Database=Testdb;User=test;Password=test;Port=3306";
-		//	cfg->connectionString = "SELECT Ip FROM allowedips";
+		strcpy(conf->context, context);
+		conf->enabled = 0;
+		strcpy(conf->connectionString, "");
+		strcpy(conf->query, "");
 	}
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "Standard MySql Whitelist configuration initialized");
-	return cfg;
+
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "Standard MySql Whitelist configuration initialized");
+	return conf;
 }
 
 
 void *mysql_whitelist_merge_dir_conf(apr_pool_t *pool, void *BASE, void *ADD)
 {
-/*	whitelist_mysql_config *base = (whitelist_mysql_config *) BASE ;
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "Specific MySql Whitelist configuration being created ...");
+	whitelist_mysql_config *base = (whitelist_mysql_config *) BASE ;
 	whitelist_mysql_config *add = (whitelist_mysql_config *) ADD ;
 	whitelist_mysql_config *conf = (whitelist_mysql_config *) mysql_whitelist_create_dir_conf(pool, "Merged configuration");
 
-	conf->enabled = ( add->enabled == 0 ) ? base->enabled : add->enabled ;
+	conf->enabled = ( add->enabled == 1 ) ? add->enabled : base->enabled ;
 
-	if (!strcasecmp(add->query, ""))
+	if (strcasecmp(add->query, "") != 0)
 		strcpy(conf->query, add->query);
 	else
 		strcpy(conf->query, base->query);
 
-	if (!strcasecmp(add->connectionString, ""))
+	if (strcasecmp(add->connectionString, "") != 0)
 		strcpy(conf->connectionString, add->connectionString);
 	else
 		strcpy(conf->connectionString, base->connectionString);
-*/
-//    conf->query = ( strcasecmp(add->query, "")) ? base->query : add->query;
-//    conf->connectionString = ( strcasecmp(add->connectionString, "")) ? base->connectionString : add->connectionString;
 
-//    ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist enabled: %d", conf->enabled);
-//    ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist connectionString: %s", conf->connectionString);
-//    ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist query: %s", conf->query);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist enabled: %d", conf->enabled);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist connectionString: %s", conf->connectionString);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist query: %s", conf->query);
 
-    return mysql_whitelist_create_dir_conf(pool, "Merged configuration");
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "Specific MySql Whitelist configuration initialized");
+
+	return conf;
 }
 
-static int whitelist_check_access(request_rec *r, int lookup_uri)
+static int whitelist_check_access(request_rec *r)
 {
-	whitelist_mysql_config *config = (whitelist_mysql_config*) ap_get_module_config(r->per_dir_config, &whitelist_mysql_module);
+	if (!server)
+		server = r->server;
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "Context: %s", config->connectionString);
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "Context: %s", config->query);
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "Context: %s", config->context);
+	whitelist_mysql_config *conf = (whitelist_mysql_config*) ap_get_module_config(r->per_dir_config, &whitelist_mysql_module);
 
-	if (config->enabled == 0)
+	if (conf == NULL)
 	{
-		ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "MySql Whitelist not enabled");
+		ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist configuration failed!!");
 		return DECLINED;
 	}
 
-	if (strcmp(config->query, "") == 0 ||
-	    strcmp(config->connectionString, "") == 0)
+	if (conf->enabled == 0)
 	{
-		ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "MySql Whitelist has no query nor connectionstring set");
+		ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist not enabled");
 		return DECLINED;
 	}
 
-	int isInList = ipaddress_is_in_list(config->connectionString, config->query, r->useragent_ip);
+	if (strcmp(conf->query, "") == 0 ||
+	    strcmp(conf->connectionString, "") == 0)
+	{
+		ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "MySql Whitelist has no query nor connectionstring set");
+		return DECLINED;
+	}
+
+	int isInList = ipaddress_is_in_list(conf->connectionString, conf->query, r->useragent_ip);
 	if (isInList == 0)
 	{
-		ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "%s not in whitelist", r->useragent_ip);
+		ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "%s not in whitelist", r->useragent_ip);
 		return HTTP_UNAUTHORIZED;
 	}
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, r->server, "Finished MySql Whitelist approved request");
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "Finished MySql Whitelist approved request");
 	return DECLINED;
 }
 
@@ -121,7 +125,7 @@ const char *whitelist_mysql_set_query(cmd_parms *cmd, void *cfg, const char *arg
         if (config)
                 strcpy(config->query, arg);
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist Query: %s", config->query);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "(module) MySql Whitelist Query: %s", config->query);
 	return NULL;
 }
 
@@ -130,9 +134,9 @@ const char *whitelist_mysql_set_connectionstring(cmd_parms *cmd, void *cfg, cons
 	whitelist_mysql_config *config = (whitelist_mysql_config *) cfg ;
 
 	if (config)
-		strcpy(config->connectionString,arg);
+		strcpy(config->connectionString, arg);
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist ConnectionString: %s", config->connectionString);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "(module) MySql Whitelist ConnectionString: %s", config->connectionString);
 	return NULL;
 }
 
@@ -142,26 +146,29 @@ const char *whitelist_mysql_set_enable(cmd_parms *cmd, void *cfg, const char *ar
 
 	if (config)
 	{
-	    if(strcasecmp(arg, "on"))
+	    if(strcmp(arg, "on") == 0)
+		config->enabled = 1;
+
+	    if(strcmp(arg, "On") == 0)
 		config->enabled = 1;
 	}
 
-	ap_log_error(APLOG_MARK, APLOG_INFO, 0, 0, "MySql Whitelist enabled: %d", config->enabled);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, server, "(module) MySql Whitelist enabled: %d", config->enabled);
 	return NULL;
 }
 
 
 static const command_rec whitelist_mysql_directives[] =
 {
-    AP_INIT_TAKE1("WhiteListMySqlConnectrionstring",  whitelist_mysql_set_connectionstring, NULL, ACCESS_CONF, "MySql Whitelist Connectionstring"),
-    AP_INIT_TAKE1("WhiteListMySqlQuery",  whitelist_mysql_set_query, NULL, ACCESS_CONF, "SQL Query to Whitelist"),
-    AP_INIT_TAKE1("WhiteListMySqlEnabled",  whitelist_mysql_set_enable, NULL, ACCESS_CONF, "MySql Whitelist enable"),
+    AP_INIT_TAKE1("WhiteListMySqlConnectrionstring",  whitelist_mysql_set_connectionstring, NULL, OR_OPTIONS, "MySql Whitelist Connectionstring"),
+    AP_INIT_TAKE1("WhiteListMySqlQuery",  whitelist_mysql_set_query, NULL, OR_OPTIONS, "SQL Query to Whitelist"),
+    AP_INIT_TAKE1("WhiteListMySqlEnabled",  whitelist_mysql_set_enable, NULL, OR_OPTIONS, "MySql Whitelist enable"),
     { NULL }
 };
 
 static void whitelist_mysql_register_hooks(apr_pool_t *p)
 {
-	ap_hook_quick_handler(whitelist_check_access, NULL, NULL, APR_HOOK_FIRST);
+	ap_hook_handler(whitelist_check_access, NULL, NULL, APR_HOOK_REALLY_FIRST);
 }
 
 module AP_MODULE_DECLARE_DATA whitelist_mysql_module = {
