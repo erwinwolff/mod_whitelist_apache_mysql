950159143f8e5472afa84927d9036cfea6a69895
Erwin Wolff <erwinwolff@hotmail.com>
2020-03-25 20:02:10 -0400

    more logical filename

--- apxs.sh
+++ apxs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/bin/apxs -l mysqlclient -n whitelist_mysql -i -a libwhitelist_mysql.so
-#/usr/bin/apxs -l mysqlclient -n blacklist_mysql -i -a -c  src/mod_blacklist_mysql.c src/CConfigUtil.cpp src/CGetIpAddressList.cpp src/CConfigUtil.h src/CGetIpAddressList.h
+/usr/bin/apxs -l mysqlclient -n whitelist_mysql -i -a whitelist_mysql.so
+#/usr/bin/apxs -l mysqlclient -n blacklist_mysql -i -a blacklist_mysql.so
